d7e8ff5e0ef5fd946ab30ab3fca801a38f1c95ad
Juan <jdlopez576@misena.edu.co>
2022-03-25 12:28:06 -0500

    Se crea una nueva clase

--- Ejercicio Areglos/Board.cs
+++ Ejercicio Areglos/Board.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ejercicio_Areglos
 {
-    public class Board
+    public class Board : Positions
     {
         private int rows { get; set; }
         private int columns { get; set; }
@@ -14,12 +14,14 @@ namespace Ejercicio_Areglos
         private List<int[]> positionToWin { get; set; }
         private Player player1 { set; get; }
         private Player player2 { set; get; }
+        
+
         public Board(int rows_, int columns_)
         {
             rows = rows_;
             columns = columns_;
             Table = new int[rows, columns];
-            positionToWin = new List<int[]>{ new int[] { 0, 1, 2 }, new int[] { 3, 4, 5 }, new int[] { 6, 7, 8 }, new int[] { 0, 3, 6 }, new int[] { 1, 4, 7 }, new int[] { 2, 5, 8 }, new int[] { 0, 4, 9 }, new int[] { 3, 4, 7} };
+            positionToWin = new List<int[]> { new int[] { 0, 1, 2 }, new int[] { 3, 4, 5 }, new int[] { 6, 7, 8 }, new int[] { 0, 3, 6 }, new int[] { 1, 4, 7 }, new int[] { 2, 5, 8 }, new int[] { 0, 4, 9 }, new int[] { 3, 4, 7 } };
         } 
         public void setValue(int value, int row, int column)
         {
---
+++ Ejercicio Areglos/Positions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Areglos
+{
+    public class Positions
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+
+        
+
+    }
+
+}
